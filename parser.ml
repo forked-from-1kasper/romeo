e33a7ef42e751b219a2f9fcae0bb666350a86272
Siegmentation Fault <siegmentationfault@yandex.ru>
2022-07-14 21:18:27 +0700

    simplify

--- parser.ml
+++ parser.ml
@@ -20,7 +20,7 @@ let atom s  = Atom s
 let node xs = Node xs
 
 let ws             = str (fun c -> c = ' ' || c = '\n' || c = '\t' || c = '\t')
-let keywords       = ["definition"; "theorem"]
+let keywords       = ["definition"; "theorem"; ":="]
 let reserved       = ['('; ')'; '\n'; '\t'; '\r'; ' ']
 let isReserved   c = List.mem c reserved
 let isntReserved c = not (List.mem c reserved)
@@ -30,10 +30,8 @@ let ident = decorateErrors ["ident"] (guard isntKeyword (str isntReserved))
 let sexp = fix (fun p -> (node <$> (ch '(' >> many ws >> many p << ch ')'))
                      <|> (atom <$> ident) << many ws)
 
-let binderIdent = decorateErrors ["binderIdent"] (str (fun c -> isntReserved c && c <> ':'))
-
 let def = token "definition" >> many ws >> ident >>=
-  fun i -> many ws >> sepBy (many ws) binderIdent >>=
+  fun i -> many ws >> sepBy (many ws) ident >>=
     fun is -> many ws >> token ":=" >> many ws >> many1 sexp >>=
       fun e -> pure (Def (i, is, Node e))
 
